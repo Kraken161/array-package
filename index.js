2b0bc586a0e35c1f29ec4461dc8332674da92932
Kraken161 <66274761+Kraken161@users.noreply.github.com>
2021-11-01 21:38:46 +0100

    commit

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 const chalk = require('chalk')
 
-const displayAll = (array) => {
-	if (typeof array !== 'object') {
-		throw new Error(`This isn't an array!`)
-	} else {
-		try {
-			console.log(chalk.red.bold('All array elements:'))
-			console.log(array)
-		} catch (e) {
-			throw new Error(e)
-		}
-	}
-}
-
-const displayAllReverse = (array) => {
-	if (typeof array !== 'object') {
-		throw new Error(`This isn't an array!`)
-	} else {
-		try {
-			console.log(chalk.red.bold('All array elements reversed:'))
-			console.log(array.reverse())
-		} catch (e) {
-			throw new Error(e)
-		}
-	}
-}
-
-const reduceElements = (array, number) => {
-	if (!number) {
-		throw new Error('Enter how many items are to be deleted.')
-	} else {
+module.exports = {
+	displayAll(array) {
 		if (typeof array !== 'object') {
 			throw new Error(`This isn't an array!`)
 		} else {
 			try {
-				console.log(chalk.red.bold('Array reduced elements:'))
-				console.log(array.reverse().splice(number))
+				console.log(chalk.red.bold('All array elements:'))
+				console.log(array)
 			} catch (e) {
 				throw new Error(e)
 			}
 		}
-	}
+	},
+
+	displayAllReverse(array) {
+		if (typeof array !== 'object') {
+			throw new Error(`This isn't an array!`)
+		} else {
+			try {
+				console.log(chalk.red.bold('All array elements reversed:'))
+				console.log(array.reverse())
+			} catch (e) {
+				throw new Error(e)
+			}
+		}
+	},
+
+	reduceElements(array, number) {
+		if (!number) {
+			throw new Error('Enter how many items are to be deleted.')
+		} else {
+			if (typeof array !== 'object') {
+				throw new Error(`This isn't an array!`)
+			} else {
+				try {
+					console.log(chalk.red.bold('Array reduced elements:'))
+					console.log(array.reverse().splice(number))
+				} catch (e) {
+					throw new Error(e)
+				}
+			}
+		}
+	},
 }
 
 // displayAll(['asdasdas', 'asdasd', 'sdf'])
